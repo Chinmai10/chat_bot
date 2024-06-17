34230c99f40f974b4db1f1f904afc9e7e6b3beb2
Chinmai10 <srichinmai2004@gmail.com>
2024-06-17 19:54:43 +0530

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [question, setQuestion] = useState(0);
+  const [answer, setAnswer] = useState(0);
 
+  async function generateAnswer() {
+    setAnswer("loading ... ") ;
+    const response = await axios({
+
+      url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyD8a1H1LfKfE3OUa1iGmdVEpur7iuTxv0U",
+      method: "post",
+      data: { contents: [{ parts: [{ text: question }] }] },
+    })
+    setAnswer(response['data']['candidates'][0]['content']['parts'][0]['text']);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Chat Bot</h1>
+      <textarea value={question} onChange={(e) => setQuestion(e.target.value)} placeholder='Ask me anything' cols="150" rows="10"></textarea>
+      <button onClick={generateAnswer}>Generate Answer</button>
+      <pre>{answer}</pre  >
     </>
+
   )
 }
 
